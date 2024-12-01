08b1ef28c70791178aa195ff83b8ec6b173e5ddf
wriddhi <wriddhihazra@gmail.com>
2024-12-02 04:20:30 +0530

    test: generate static metadata

--- src/app/[handle]/page.tsx
+++ src/app/[handle]/page.tsx
@@ -1,31 +1,31 @@
 import { getProfile, getPosts } from "@/actions/data/get";
 import { Receipt, type Props as ReceiptProps } from "@/app/_components/Receipt";
 import { Container } from "@/components/Container";
-import { Metadata } from "next";
+// import { Metadata } from "next";
 
 type Props = {
   params: Promise<{ handle: string }>;
 };
 
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const { handle } = await params;
+// export async function generateMetadata({ params }: Props, parentsP): Promise<Metadata> {
+//   const { handle } = await params;
 
-  const { profile, error } = await getProfile(handle);
+//   const { profile, error } = await getProfile(handle);
 
-  if (error || !profile || !profile.success) {
-    return {
-      title: `Bskypt | Error`,
-      description: `You entered an invalid handle.`,
-    };
-  }
+//   if (error || !profile || !profile.success) {
+//     return {
+//       title: `Bskypt | Error`,
+//       description: `You entered an invalid handle.`,
+//     };
+//   }
 
-  return {
-    title: `Bskypt | Receipt for @${profile.data?.displayName ?? handle}`,
-    description: `Checkout this cool social receipt for @${
-      profile.data?.displayName ?? handle
-    }`,
-  };
-}
+//   return {
+//     title: `Bskypt | Receipt for @${profile.data?.displayName ?? handle}`,
+//     description: `Checkout this cool social receipt for @${
+//       profile.data?.displayName ?? handle
+//     }`,
+//   };
+// }
 
 export default async function Page({ params }: Props) {
   const { handle } = await params;
